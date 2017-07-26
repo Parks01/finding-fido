d792bb02283ab9d7d8466fedff0aa5c78421c017
Kaili Nishihira, Kim Schulze and Lois Choi <kim@>
2017-07-26 10:00:48 -0700

    Fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -59,45 +59,36 @@ function fillDogsData()
 
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 	fillDogsData();
 
-	$("#searchAll").click(function()
-	{ $("#show-all-results").empty();
-	var i = 0;
-		for (var j = 0; j < dogs.length; j++)
-		{
-			$("#show-all-results").append("<div class='row'>")
-			for (var k = 0; k < 3; k++)
-			{
-				if (i < dogs.length) {
+	$("#searchAll").click(function() {
+		$("#show-all-results").empty();
+		var i = 0;
+		for (var j = 0; j < dogs.length; j++)	{
+			if (i < dogs.length) {
 				$("#show-all-results").append("<div class='col-md-4'>" + "<img src='" + dogs[i].dogImage + "'/>'" + "<br><strong>" + dogs[i].dogName + "</strong><br>" + dogs[i].age + "<br>" + dogs[i].gender + "</div>");
 				i++;
-				}
 			}
-				$("#show-all-results").append("</div>")
-			}
+		}
 	});
 
 	$("#dogSearchFilter").submit(function(event) {
-			event.preventDefault();
-			$("#show-all-results").empty();
+		event.preventDefault();
+		$("#show-all-results").empty();
 
-			var filterAge = [];
-			$("input:checkbox[name=dogAge]:checked").each(function(){
-      	filterAge.push($(this).val());
+		var filterAge = [];
+		$("input:checkbox[name=dogAge]:checked").each(function() {
+    	filterAge.push($(this).val());
 
-				for(var i=0; i< dogs.length; i++)
-			{
-				for(var j=0; j< filterAge.length ; j++)
-				{
-					if(dogs[i].age === filterAge[j])
-					{
-						$("#show-all-results").append("<table><tr colspan=\"2\"><div id=\"show-image\"><img src='" + dogs[i].dogImage + "'/></div></tr><tr><th colspan=\"2\" class=\"dog-name\"><strong>" + dogs[i].dogName + "</strong></th></tr><tr><td>Age: " + dogs[i].age + "</td><td class=\"age\"></td></tr><tr><td>Gender: " + dogs[i].gender + "</td><td class=\"gender\"></td></tr></table>")
+			for (var i = 0; i < dogs.length; i++) {
+				for (var j = 0; j < filterAge.length; j++) {
+					if (dogs[i].age === filterAge[j]) {
+						$("#show-all-results").append("<div class='col-md-4'>" + "<img src='" + dogs[i].dogImage + "'/>'" + "<br><strong>" + dogs[i].dogName + "</strong><br>" + dogs[i].age + "<br>" + dogs[i].gender + "</div>");
 					}
 				}
 			}
+		});
 	});
-});
 });
